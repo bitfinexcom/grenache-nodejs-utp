10eb184e9b5a92630eb9ea00328ddc2b891d2403
Robert Kowalski <rok@kowalski.gd>
2018-05-09 21:16:18 +0200

    woah

---
+++ examples/connect.js
@@ -0,0 +1,28 @@
+exports.register = register
+function register (peer, isServer, cb) {
+  peer.request('etoro_broker', {
+    service: 'etoro_cris',
+    server: isServer
+  }, { timeout: 10000 }, (err, data) => {
+    if (err) return cb(err)
+
+    const pool = Object.keys(data['etoro_cris'])
+
+    const clients = pool.filter((el) => {
+      const tmp = data['etoro_cris'][el]
+
+      const isClient = !isServer
+      if (isClient) {
+        return tmp.server
+      }
+
+      return !tmp.server
+    })
+
+    const res = clients.map((el) => {
+      return data['etoro_cris'][el]
+    })
+
+    cb(null, res)
+  })
+}
--- examples/nat_server.js
+++ examples/nat_server.js
@@ -2,32 +2,65 @@
 
 const Grenache = require('./../')
 const Link = require('grenache-nodejs-link')
-const Peer = Grenache.PeerRPCServer
+const { URL } = require('url')
+const { register } = require('./connect.js')
 
 const link = new Link({
   grape: 'http://127.0.0.1:30001'
 })
 link.start()
 
-const peer = new Peer(link, {
-  timeout: 300000
+const peer = new Grenache.PeerRPCClient(link, {
+  maxActiveKeyDests: 3
 })
 peer.init()
 
-const port = 13378
-const service = peer.transport('server')
+const peerSrv = new Grenache.PeerRPCServer(link, {
+  timeout: 600000
+})
+
+const port = 0
+const service = peerSrv.transport('server')
 service.listen(port)
 
 setInterval(function () {
   link.announce('etoro_cris', service.port, {})
+  console.log("announce", service.port)
 
-  // establish a mapping for port 13378
-  service.socket.send(Buffer('hello'), 0, 5, port, 'no-reply.bitfinex.com')
+  register(peer, true, (err, res) => {
+    if (err && err.message === 'ERR_TIMEOUT') return
+    else if (err) throw err
+
+    connect(peer, res)
+  })
 }, 1000)
 
-service.on('request', (rid, key, payload, handler, cert, additional) => {
-  handler.reply(null, 'strategy is everything')
+function connect (peer, clients) {
+  if (!clients) return
 
-  // console.log(`got reply from to ${payload.address}:${additional.port}`)
-  // console.log(`now connecting through NAT to ${payload.address}:${payload.httpPort}`)
+  const el = clients.pop()
+
+  if (!el) return
+
+  console.log(`connecting to ${el.address}:${el.port} ...`)
+
+  const { port, address } = el
+
+  const buf = Buffer('ÿ')
+  console.log(`socket.send(${buf}, 0, ${buf.length}, ${+port}, ${address})`)
+
+  const socket = peer.transport('client').getSocket()
+  socket.send(buf, 0, buf.length, port, address)
+
+  connect(clients)
+}
+
+service.on('request', (rid, key, payload, handler, cert, additional) => {
+  // because of shared socket, all responses arrive here
+  if (payload.etoro_cris) return
+
+  console.log(payload)
+  if (!payload.strategy) return
+
+  handler.reply(null, 'strategy is everything')
 })
